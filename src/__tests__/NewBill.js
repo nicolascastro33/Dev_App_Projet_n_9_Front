9aa864a436a9c8b17042a27395d8c305178debed
Nicolas Castro <castro.nicolasalain@gmail.com>
2024-02-09 18:54:50 +0100

    Beginning MEP of the NewBill test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,9 +2,168 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import mockStore from "../__mocks__/store"
+import ArrowIcon from '../assets/svg/arrow.js'
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import store from "../__mocks__/store";
+
+jest.mock("../app/store", () => mockStore)
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then I upload a valid file", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then I upload an unvalid file", () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const html = NewBillUI()
+      document.body.innerHTML = html
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const newBill = new NewBill({
+        document, onNavigate, store, localStorage: window.localStorage
+      })
+
+      const inputUploadFile = screen.getByTestId('file')
+      inputUploadFile.files[0] = ArrowIcon
+
+      expect(inputUploadFile.files[0]).toBe()
+
+      const handleClickUploadFile = jest.fn(newBill.handleChangeFile)
+      inputUploadFile.addEventListener('change', handleClickUploadFile)
+      fireEvent.change()
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
+
+
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then ...", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      //to-do write assertion
+    })
+  })
+})
 
 
 describe("Given I am connected as an employee", () => {
